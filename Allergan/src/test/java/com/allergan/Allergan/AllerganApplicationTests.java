47f1b6ccbbc4487e1a1e9276e129e0684b2113e1
Adwait Mayekar <adwaitmayekar25@gmail.com>
2022-03-04 11:27:02 +0530

    Allergan Project

---
+++ Allergan/src/test/java/com/allergan/Allergan/AllerganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allergan.Allergan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllerganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
